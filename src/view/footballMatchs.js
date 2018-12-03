c0c9837f43066b42aa8f445b489c95663558474b
HEffatpanah <h.effatpanah@gmail.com>
2018-12-04 00:49:08 +0330

    save component to avoid re-rendering

--- src/view/footballMatchs.js
+++ src/view/footballMatchs.js
@@ -18,10 +18,17 @@ export default  class matche extends Component {
 
     render() {
         console.log(this.state.matchInformation['card']);
-        console.log(this.state.matchInformation['goal']);
-
+        console.log(this.state.matchInformation['component']);
+        let match=this.state.matchInformation['component'];
+        if(this.state.matchInformation['component']===undefined) {
+            match = <div>salam</div>;
+            let tmp = this.state.matchInformation;
+        tmp['component'] = match;
+            this.setState({matchInformation: tmp});
+        }
+        // match=<Menu m={{matchInformation: this.state.matchInformation, summary:false}}/>
         return (
-            <Menu m={{matchInformation: this.state.matchInformation, summary:false}}/>
+            <div>{match}</div>
         );
     }
 }
