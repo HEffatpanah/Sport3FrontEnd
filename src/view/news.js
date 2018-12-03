d53bc6e104f08fc1069ba01231d8c6bcf365822b
hossein <24716475+heffatpanah@users.noreply.github.com>
2018-12-03 22:11:52 +0330

    new template teams and news

--- src/view/news.js
+++ src/view/news.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Template from '../components/template'
+import {Grid, Segment} from 'semantic-ui-react'
 
 
 
@@ -11,18 +12,42 @@ class App extends Component {
     }
 
     render() {
-        const advertise = <div>advertise</div>;
-        const news = <div>The Strong Hossein is back</div>;
-        const news_image = <img href={'./images/horse.jpg'}/>;
-        const other_news = <div>
-            <div><a>Hossein1</a></div>
-            <div><a>Hossein2</a></div>
-            <div><a>Hossein3</a></div>
-            <div><a>Hossein4</a></div>
-        </div>;
+        const advertise = <Segment>advertise</Segment>;
+        const news_image = <img src={require("../images/horse.jpg")} style={{"width":"12vw","height":"12vh","float": "right"}}/>;
+        const news = <Segment style={{"overflow":"auto"}}>
+            {news_image}
+            <header style={{"text-decoration": "underline"}}><h2>Strong Hossein</h2></header>
+            <body>Strong Hossein is back</body>
+            </Segment>;
+            const other_news=
+            <Segment>
+
+                <a id="ew" href="https://www.google.com">news</a><br/>
+                <a href="https://www.google.com">ews</a><br/>
+                <a href="https://www.google.com">news</a><br/>
+                <a href="https://www.google.com">news</a><br/>
+                <a href="https://www.google.com">news</a><br/>
+                <a href="https://www.google.com">news</a><br/>
+                <a href="https://www.google.com">news</a><br/>
+                <a href="https://www.google.com">news</a><br/>
+            </Segment>;
+                const body =
+            <Grid style={{width:'100%'}}>
+                <Grid.Row columns={3}>
+                    <Grid.Column width={4}>
+                        {advertise}
+                    </Grid.Column>
+                    <Grid.Column width={8}>
+                        {news}
+                    </Grid.Column>
+                    <Grid.Column width={4}>
+                        {other_news}
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>;
         return (
             <div>
-                <Template {...this.props} template={{column_1: advertise, column_2: news_image, column_3: other_news}}/>
+                <Template {...this.props} template={{body:body}}/>
             </div>);
     }
 }
