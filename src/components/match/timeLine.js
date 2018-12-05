da8c8f6e4770394e86b61d81f92c0b1e4b9a80e5
aliasgharkhani <khani.ali1376@gmail.com>
2018-12-05 16:28:11 +0330

    signup completed unless check box

--- src/components/match/timeLine.js
+++ src/components/match/timeLine.js
@@ -1,52 +1,52 @@
-
-import React,{Component} from "react";
-
-import 'react-vertical-timeline-component/style.min.css';
-import Timeline from 'react-horizontal-timeline';
-
-const data = [
-  { name: "Wake up", dataType: "user-data", date: "2012-09-28T07:00:00", isSelected: false, isNotPermitted: false, r: 7, cy: 50, cx: 0, activity: { name: "Example Activity A", startTime: "2012-09-28T06:50:00", endTime: "2012-09-28T11:45:00" }  },
-  { name: "Breakfast", dataType: "user-data", date: "2012-09-28T07:30:00", isSelected: false, isNotPermitted: false, r: 7, cy: 50, cx: 0, activity: { name: "Example Activity A", startTime: "2012-09-28T06:50:00", endTime: "2012-09-28T11:45:00" } },
-  { name: "Leave kids at school", dataType: "user-data", date: "2012-09-28T07:45:00", isSelected: false, isNotPermitted: false, r: 7, cy: 50, cx: 0, activity: { name: "Example Activity A", startTime: "2012-09-28T06:50:00", endTime: "2012-09-28T11:45:00" } },
-  { name: "Check email", dataType: "user-data", date: "2012-09-28T08:00:00", isSelected: false, isNotPermitted: false, r: 7, cy: 50, cx: 0, activity: { name: "Example Activity A", startTime: "2012-09-28T06:50:00", endTime: "2012-09-28T11:45:00" } },
-  { name: "Lunch", dataType: "user-data", date: "2012-09-28T11:30:00", isSelected: false, isNotPermitted: false, r: 7, cy: 50, cx: 0, activity: { name: "Example Activity A", startTime: "2012-09-28T06:50:00", endTime: "2012-09-28T11:45:00" } },
-  { name: "Send report", dataType: "user-data", date: "2012-09-28T13:15:00", isSelected: false, isNotPermitted: true, r: 7, cy: 50, cx: 0, activity: null },
-  { name: "Pick kids", dataType: "user-data", date: "2012-09-28T17:16:00", isSelected: false, isNotPermitted: true, r: 7, cy: 50, cx: 0, activity: null },
-  { name: "Dinner", dataType: "user-data", date: "2012-09-28T18:13:00", isSelected: false, isNotPermitted: false, r: 7, cy: 50, cx: 0, activity: { name: "Example Activity B", startTime: "2012-09-28T17:45:00", endTime: "2012-09-28T20:30:00" } },
-  { name: "Watch a movie", dataType: "user-data", date: "2012-09-28T20:16:00", isSelected: false, isNotPermitted: false, r: 7, cy: 50, cx: 0, activity: { name: "Example Activity B", startTime: "2012-09-28T17:45:00", endTime: "2012-09-28T20:30:00" } },
-  { name: "Go to sleep", dataType: "user-data", date: "2012-09-28T22:00:00", isSelected: false, isNotPermitted: true, r: 7, cy: 50, cx: 0, activity: null },
-  { name: "Example system event A", dataType: "system-data", date: "2012-09-28T14:30:20", isSelected: false, isNotPermitted: true, y: 42, cx: 0 },
-  { name: "Example system event B", dataType: "system-data", date: "2012-09-28T17:45:00", isSelected: false, isNotPermitted: true, y: 42, cx: 0 },
-  { name: "Example system event C", dataType: "system-data", date: "2012-09-28T19:00:15", isSelected: false, isNotPermitted: false, y: 42, cx: 0 },
-  { name: "Example system event D", dataType: "system-data", date: "2012-09-28T22:20:00", isSelected: false, isNotPermitted: false, y: 42, cx: 0 }
-]
-
-const activityDataSample = [
-  undefined,
-  { name: "Example Activity A", startTime: "2012-09-28T06:50:00", endTime: "2012-09-28T11:45:00" },
-  { name: "Example Activity B", startTime: "2012-09-28T10:50:00", endTime: "2012-09-28T13:45:00" },
-  { name: "Example Activity C", startTime: "2012-09-28T16:50:00", endTime: "2012-09-28T17:45:00" }
-]
-
-export default class App extends Component {
-
-  render() {
-    return (
-      <div>
-        {/*<div className="App">*/}
-          {/*<header className="App-header">*/}
-            {/*/!*<img src={logo} className="App-logo" alt="logo" />*!/*/}
-            {/*<h1 className="App-title">Welcome to React</h1>*/}
-          {/*</header>*/}
-        {/*</div>*/}
-
-        <Timeline
-          data={data}
-          activityObject={activityDataSample[0]}
-          eventTypeObject={{userEvents: true, systemEvents: true}}
-        />
-
-      </div>
-    );
-  }
-}
+//
+// import React,{Component} from "react";
+//
+// import 'react-vertical-timeline-component/style.min.css';
+// import Timeline from 'react-horizontal-timeline';
+//
+// const data = [
+//   { name: "Wake up", dataType: "user-data", date: "2012-09-28T07:00:00", isSelected: false, isNotPermitted: false, r: 7, cy: 50, cx: 0, activity: { name: "Example Activity A", startTime: "2012-09-28T06:50:00", endTime: "2012-09-28T11:45:00" }  },
+//   { name: "Breakfast", dataType: "user-data", date: "2012-09-28T07:30:00", isSelected: false, isNotPermitted: false, r: 7, cy: 50, cx: 0, activity: { name: "Example Activity A", startTime: "2012-09-28T06:50:00", endTime: "2012-09-28T11:45:00" } },
+//   { name: "Leave kids at school", dataType: "user-data", date: "2012-09-28T07:45:00", isSelected: false, isNotPermitted: false, r: 7, cy: 50, cx: 0, activity: { name: "Example Activity A", startTime: "2012-09-28T06:50:00", endTime: "2012-09-28T11:45:00" } },
+//   { name: "Check email", dataType: "user-data", date: "2012-09-28T08:00:00", isSelected: false, isNotPermitted: false, r: 7, cy: 50, cx: 0, activity: { name: "Example Activity A", startTime: "2012-09-28T06:50:00", endTime: "2012-09-28T11:45:00" } },
+//   { name: "Lunch", dataType: "user-data", date: "2012-09-28T11:30:00", isSelected: false, isNotPermitted: false, r: 7, cy: 50, cx: 0, activity: { name: "Example Activity A", startTime: "2012-09-28T06:50:00", endTime: "2012-09-28T11:45:00" } },
+//   { name: "Send report", dataType: "user-data", date: "2012-09-28T13:15:00", isSelected: false, isNotPermitted: true, r: 7, cy: 50, cx: 0, activity: null },
+//   { name: "Pick kids", dataType: "user-data", date: "2012-09-28T17:16:00", isSelected: false, isNotPermitted: true, r: 7, cy: 50, cx: 0, activity: null },
+//   { name: "Dinner", dataType: "user-data", date: "2012-09-28T18:13:00", isSelected: false, isNotPermitted: false, r: 7, cy: 50, cx: 0, activity: { name: "Example Activity B", startTime: "2012-09-28T17:45:00", endTime: "2012-09-28T20:30:00" } },
+//   { name: "Watch a movie", dataType: "user-data", date: "2012-09-28T20:16:00", isSelected: false, isNotPermitted: false, r: 7, cy: 50, cx: 0, activity: { name: "Example Activity B", startTime: "2012-09-28T17:45:00", endTime: "2012-09-28T20:30:00" } },
+//   { name: "Go to sleep", dataType: "user-data", date: "2012-09-28T22:00:00", isSelected: false, isNotPermitted: true, r: 7, cy: 50, cx: 0, activity: null },
+//   { name: "Example system event A", dataType: "system-data", date: "2012-09-28T14:30:20", isSelected: false, isNotPermitted: true, y: 42, cx: 0 },
+//   { name: "Example system event B", dataType: "system-data", date: "2012-09-28T17:45:00", isSelected: false, isNotPermitted: true, y: 42, cx: 0 },
+//   { name: "Example system event C", dataType: "system-data", date: "2012-09-28T19:00:15", isSelected: false, isNotPermitted: false, y: 42, cx: 0 },
+//   { name: "Example system event D", dataType: "system-data", date: "2012-09-28T22:20:00", isSelected: false, isNotPermitted: false, y: 42, cx: 0 }
+// ]
+//
+// const activityDataSample = [
+//   undefined,
+//   { name: "Example Activity A", startTime: "2012-09-28T06:50:00", endTime: "2012-09-28T11:45:00" },
+//   { name: "Example Activity B", startTime: "2012-09-28T10:50:00", endTime: "2012-09-28T13:45:00" },
+//   { name: "Example Activity C", startTime: "2012-09-28T16:50:00", endTime: "2012-09-28T17:45:00" }
+// ]
+//
+// export default class App extends Component {
+//
+//   render() {
+//     return (
+//       <div>
+//         {/*<div className="App">*/}
+//           {/*<header className="App-header">*/}
+//             {/*/!*<img src={logo} className="App-logo" alt="logo" />*!/*/}
+//             {/*<h1 className="App-title">Welcome to React</h1>*/}
+//           {/*</header>*/}
+//         {/*</div>*/}
+//
+//         <Timeline
+//           data={data}
+//           activityObject={activityDataSample[0]}
+//           eventTypeObject={{userEvents: true, systemEvents: true}}
+//         />
+//
+//       </div>
+//     );
+//   }
+// }
--- src/view/signup.js
+++ src/view/signup.js
@@ -1,28 +1,83 @@
 import React, {Component} from 'react'
 import {Button, Form, Grid, Segment} from 'semantic-ui-react'
 import Template from '../components/template'
-const FormExampleInverted = () => (
-  <Segment inverted>
-    <Form inverted>
-      <Form.Group widths='equal'>
-        <Form.Input fluid label='First name' placeholder='First name' />
-        <Form.Input fluid label='Last name' placeholder='Last name' />
-      </Form.Group>
-      <Form.Checkbox label='I agree to the Terms and Conditions' />
-      <Button type='submit'>Submit</Button>
-    </Form>
-  </Segment>
-)
+import {findDOMNode} from 'react-dom';
+
+
+
+class SignupForm extends Component{
+    constructor(props) {
+        super(props);
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.name = this.name.bind(this)
+        this.passConfirmCheck= this.passConfirmCheck.bind(this)
+        this.passSave = this.passSave.bind(this)
+
+    }
+    state = {
+        equal:true,
+        pass:null
+    };
+    name(e){
+        let user = findDOMNode(e.refs.uname).value.trim()
+        let pass = findDOMNode(e.refs.pass).value.trim()
+        console.log(user, pass)
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+
+        console.log(event.target.uname.value)
+        console.log(event.target.pass.value)
+        console.log(event.target.email.value)
+        // console.log(e)
+        // alert('asf');
+    }
+    passConfirmCheck(event){
+        if(this.state.pass === event.target.value){
+            this.setState({equal:true})
+        }
+        else
+            this.setState({equal:false})
+    }
+    passSave(event){
+        this.setState({pass:event.target.value})
+    }
+    render(){
+        return(
+            <Segment >
+                <Form onSubmit={this.handleSubmit}>
+
+                    <Form.Input name="fname" fluid label='First name' placeholder='First name' required/>
+                    <Form.Input name="lname" fluid label='Last name' placeholder='Last name' required/>
+                    <Form.Input name="uname" fluid label='User name' placeholder='User name' required/>
+                    <Form.Input name="pass" fluid label='Password' placeholder='Password' required onChange={this.passSave} type="password"/>
+                    <Form.Input name="Cpass" fluid label='Confirm Password' placeholder='Confirm Password' required onChange={this.passConfirmCheck} error={!this.state.equal} type="password"/>
+                    <Form.Input name="email" fluid label='Email' placeholder='Email@example.com' type="email" required/>
+                    <Form.Checkbox  label='I agree to the Terms and Conditions' required/>
+                    <Button color='teal' fluid type='submit'>Submit</Button>
+                </Form>
+            </Segment>
+        )
+    }
+
+
+};
 
 
 class App extends Component{
     render() {
         const body =
-            <Grid>
-
-            </Grid>
+            <Grid verticalAlign='middle' textAlign='center' style={{width:'100%', height: '100%'}}>
+                <Grid.Row>
+                    <Grid.Column style={{maxWidth: '30vw'}} >
+                        <SignupForm/>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>;
         return (
-            <Template body={body}/>
+            <Template {...this.props} body={body}/>
         );
     }
 }
+export default App;
