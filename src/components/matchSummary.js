aa4ef5a42c1d372b1e98725ae3c41092da83aa0e
aliasgharkhani <khani.ali1376@gmail.com>
2019-02-01 22:12:43 +0330

    last version

--- src/components/matchSummary.js
+++ src/components/matchSummary.js
@@ -11,7 +11,8 @@ export default class MatchesSummaryTable extends Component {
         this.state = {
             column: null,
             direction: null,
-            matchesData: this.props.matchesData['tableBody'][0]['matches'],
+            mainMatchesData: this.props.matchesData['tableBody'][0]['matches'],
+            favoriteMatchesData: this.props.matchesData['tableBody'][1]['matches'],
         };
         this.redirect_to_match = this.redirect_to_match.bind(this)
     }
@@ -31,26 +32,41 @@ export default class MatchesSummaryTable extends Component {
         if (this.state.column !== clickedColumn) {
 
             this.setState({
-                matchesData: _.sortBy(matches, [clickedColumn]),
+                mainMatchesData: _.sortBy(matches, [clickedColumn]),
                 column: clickedColumn,
                 direction: 'ascending'
             });
-            console.log(this.state.matchesData);
+            console.log(this.state.mainMatchesData);
             return
         }
         this.setState({
-            matchesData: matches.reverse(),
+            mainMatchesData: matches.reverse(),
+            direction: this.state.direction === 'ascending' ? 'descending' : 'ascending',
+        })
+    };
+
+    handleSort1 = clickedColumn => () => {
+        const {matches, league_season} = this.props.matchesData['tableBody'][1];
+        if (this.state.column !== clickedColumn) {
+
+            this.setState({
+                mainMatchesData: _.sortBy(matches, [clickedColumn]),
+                column: clickedColumn,
+                direction: 'ascending'
+            });
+            return
+        }
+        this.setState({
+            favoriteMatchesData: matches.reverse(),
             direction: this.state.direction === 'ascending' ? 'descending' : 'ascending',
         })
     };
 
 
-    getTableData() {
+    getTableMainData() {
         let active = true;
-        // const tableBody = this.props.matchesData['tableBody'].map((session) => {
-        // if ((session['league_season'] === this.props.session) || this.props.session === 'all') {
         const tableBody =
-            this.state.matchesData.map((data) => {
+            this.state.mainMatchesData.map((data) => {
                 active = !active;
                 let result;
                 if (data['date'] === 'فردا')
@@ -58,16 +74,16 @@ export default class MatchesSummaryTable extends Component {
                 else
                     result = data['team2Goal'] + '-' + data['team1Goal'];
                 return (
-                    <Table.Row onClick={this.redirect_to_match} mode={data['matchLink']} active={active}
+                    <Table.Row active={active}
                                style={{'width': '100%'}}>
-                        {/*<a href={data['matchLink']}>*/}
+                        <a href={data['matchLink']}>
                         <Table.Cell width={2}><a href={data['team1Link']}
                                                  target='_blank'>{data['team1Name']}</a></Table.Cell>
                         <Table.Cell width={1}>{result}</Table.Cell>
                         <Table.Cell width={2}><a href={data['team2Link']}
                                                  target='_blank'>{data['team2Name']}</a></Table.Cell>
                         <Table.Cell width={1}>{data['date']}</Table.Cell>
-                        {/*</a>*/}
+                        </a>
                     </Table.Row>
                 )
             })
@@ -99,19 +115,73 @@ export default class MatchesSummaryTable extends Component {
         )
     };
 
+
+    getTableFavoriteData() {
+        let active = true;
+        const tableBody =
+            this.state.favoriteMatchesData.map((data) => {
+                active = !active;
+                let result;
+                if (data['date'] === 'فردا')
+                    result = '-';
+                else
+                    result = data['team2Goal'] + '-' + data['team1Goal'];
+                return (
+                    <Table.Row active={active}
+                               style={{'width': '100%'}}>
+                        <a href={data['matchLink']}>
+                        <Table.Cell width={2}><a href={data['team1Link']}
+                                                 target='_blank'>{data['team1Name']}</a></Table.Cell>
+                        <Table.Cell width={1}>{result}</Table.Cell>
+                        <Table.Cell width={2}><a href={data['team2Link']}
+                                                 target='_blank'>{data['team2Name']}</a></Table.Cell>
+                        <Table.Cell width={1}>{data['date']}</Table.Cell>
+                        </a>
+                    </Table.Row>
+                )
+            });
+
+        // }
+        // );
+        const tableHeader = this.props.matchesData['tableHeader'].map((data) => {
+                if (data === 'نتیجه')
+                    return (
+                        <Table.HeaderCell style={{backgroundColor: '#bc001a', color: 'white'}}> {data}</Table.HeaderCell>
+                    );
+                else
+                    return (
+                        <Table.HeaderCell sorted={this.state.column === data ? this.state.direction : null}
+                                          onClick={this.handleSort1(data)}
+                                          style={{backgroundColor: '#bc001a', color: 'white'}}> {data}</Table.HeaderCell>
+                    );
+            }
+        );
+        return (
+            <Table sortable celled fixed>
+                <Table.Header>
+                    <Table.Row>
+                        {tableHeader}
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>{tableBody}</Table.Body>
+            </Table>
+        )
+    };
+
+
+
     render() {
-        console.log('lkkj');
         const all = <Translate id="all"/>;
         const fav = <Translate id="favorite"/>;
         const panes = [
             {
-                menuItem: {key: 'all', content: all}, render: () => <Tab.Pane>{this.getTableData()}</Tab.Pane>
+                menuItem: {key: 'all', content: all}, render: () => <Tab.Pane>{this.getTableMainData()}</Tab.Pane>
 
             },
             {
                 menuItem: {key: 'favorite', content: fav},
                 render: () => <Tab.Pane>
-                    <div>Mahdi</div>
+                    <div>{this.getTableFavoriteData()}</div>
                 </Tab.Pane>
             },
         ];
